65884f750b97be4f1de523447b479b0e08fb16d0
cseswapon <cseswaponsaha@gmail.com>
2026-06-10 15:37:44 +0600

    [ADD] - added on intial project

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig({
+  files: ["**/*.{js,ts}"],
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+    tseslint.configs.recommended,
+  ],
+});
